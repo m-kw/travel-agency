9c6794385aae279a1852bdc1f3aa9d7833abcc01
Magda Kwiatkowska-Witoń <magda-kwiatkowska@wp.pl>
2019-11-28 12:32:25 +0100

    Add routing to Trips, Trip, Countries, Country

--- src/App.js
+++ src/App.js
@@ -7,11 +7,12 @@ import MainLayout from './components/layout/MainLayout/MainLayout';
 
 import Home from './components/views/Home/Home';
 import Trips from './components/views/Trips/TripsContainer';
-
 import Info from './components/views/Info/Info';
 import NotFound from './components/views/NotFound/NotFound';
-
-
+import Country from './components/views/Country/CountryContainer';
+import Trip from './components/views/Trip/TripContainer';
+import Countries from './components/views/Countries/CountriesContainer';
+import Regions from './components/views/Regions/RegionsContainer';
 
 import parseTrips from './utils/parseTrips';
 import { setMultipleStates } from './redux/globalRedux';
@@ -42,9 +43,11 @@ class App extends React.Component {
           <Switch location={location}>
             <Route exact path='/' component={Home} />
             <Route exact path='/trips' component={Trips} />
-
             <Route exact path='/info' component={Info} />
-
+            <Route exact path='/countries' component={Countries} />
+            <Route exact path='/regions' component={Regions} />
+            <Route exact path='/country/:id' component={Country} />
+            <Route exact path='/trip/:id' component={Trip} />
             <Route path='*' component={NotFound} />
           </Switch>
         </MainLayout>
