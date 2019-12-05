1aa651d1f4495a808885a6b2d784558d7762b292
Magda Kwiatkowska-Witoń <magda-kwiatkowska@wp.pl>
2019-12-05 11:25:07 +0100

    Add displaying accommodation options - OrderOptionIcons. TODO: toggle active class on click

--- src/components/features/OrderOption/OrderOptionIcons.js
+++ src/components/features/OrderOption/OrderOptionIcons.js
@@ -1,11 +1,39 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import Icon from '../../common/Icon/Icon';
+import { formatPrice } from '../../../utils/formatPrice';
 
-const OrderOptionIcons = () => {
+import styles from './OrderOption.scss';
+
+const OrderOptionIcons = ({ values, required, setOptionValue, currentValue }) => {
   return (
-    <div>
-      <p>OrderOptionIcons</p>
+    <div className={styles.icon}>
+
+      {required ? '' : (
+        <div onChange={setOptionValue('')}>
+          <Icon name='times-circle' /> none
+        </div>
+      )}
+
+      {values.map(value => {
+        console.log('value', value);
+        return (
+          <div className={currentValue ? styles.iconActive : styles.icon} key={value.id} onClick={() => setOptionValue(value.id)}>
+            <Icon name={value.icon} />
+            {value.name} {formatPrice(value.price)}
+          </div>
+        );
+      })}
+
     </div>
   );
+};
+
+OrderOptionIcons.propTypes = {
+  required: PropTypes.bool,
+  values: PropTypes.array,
+  setOptionValue: PropTypes.func,
+  currentValue: PropTypes.string,
 };
 
 export default OrderOptionIcons;
