8f1236d757bea4c681cc01d30304e2b0bc6cec0e
Magda Kwiatkowska-Witoń <magda-kwiatkowska@wp.pl>
2019-12-08 23:28:25 +0100

    Add displaying options checkboxes, cannot calculate total price

--- src/components/features/OrderOption/OrderOptionCheckboxes.js
+++ src/components/features/OrderOption/OrderOptionCheckboxes.js
@@ -1,11 +1,43 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './OrderOption.scss';
+import { formatPrice } from '../../../utils/formatPrice';
 
-const OrderOptionCheckboxes = () => {
+const newValueSet = (currentValue, id, checked) => {
+  if (checked) {
+    return [
+      ...currentValue,
+      id,
+    ];
+  } else {
+    return currentValue.filter(value => value != id);
+  }
+};
+
+const OrderOptionCheckboxes = ({ values, currentValue, setOptionValue }) => {
+  console.log('values', values);
   return(
-    <div>
-      <p>OrderOptionCheckboxes</p>
+    <div className={styles.checkboxes}>
+      {values.map(value => (
+        <label key={value.id}>
+          <input
+            value={value.id}
+            type='checkbox'
+            onChange={e => {
+              console.log('currentValue', currentValue);
+              setOptionValue(newValueSet(currentValue, value.id, e.currentTarget.checked));
+            }}
+          />{value.name} {formatPrice(value.price)}
+        </label>
+      ))}
     </div>
   );
+};
+OrderOptionCheckboxes.propTypes = {
+  values: PropTypes.array,
+  price: PropTypes.number,
+  currentValue: PropTypes.array,
+  setOptionValue: PropTypes.func,
 };
 
 export default OrderOptionCheckboxes;
