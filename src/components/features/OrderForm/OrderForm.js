3abc8ad47df8a2f873faa2c19b056af6bcacd0b1
Magda Kwiatkowska-Witoń <magda-kwiatkowska@wp.pl>
2019-12-09 15:13:02 +0100

    Remove defracturing of options in OrderForm component

--- src/components/features/OrderForm/OrderForm.js
+++ src/components/features/OrderForm/OrderForm.js
@@ -6,16 +6,16 @@ import PropTypes from 'prop-types';
 import pricing from '../../../data/pricing.json';
 import OrderOption from '../OrderOption/OrderOption';
 
-const OrderForm = ({ tripCost, setOrderOption, ...options }) => {
+const OrderForm = ({ tripCost, setOrderOption, options }) => {
   return (
     <Row>
       {pricing.map((option) => (
         <Col md={4} key={option.id}>
-          <OrderOption {...option} currentValue={options.options[option.id]} setOrderOption={setOrderOption}/>
+          <OrderOption {...option} currentValue={options[option.id]} setOrderOption={setOrderOption}/>
         </Col>
       ))}
       <Col xs={12}>
-        <OrderSummary tripCost={tripCost} {...options}/>
+        <OrderSummary tripCost={tripCost} options={options}/>
       </Col>
     </Row>
   );
