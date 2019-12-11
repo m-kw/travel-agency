edb57c33f9ebb1aead9c9dc52ae5528254c4de18
Magda Kwiatkowska-Witoń <magda-kwiatkowska@wp.pl>
2019-12-11 14:35:20 +0100

    Add testing classNames

--- src/components/layout/Hero/Hero.test.js
+++ src/components/layout/Hero/Hero.test.js
@@ -5,7 +5,7 @@ import Hero from './Hero';
 describe('Component Hero', () => {
   it('should render without crashing', () => {
     //const component = shallow(<Hero  />);
-    const component = shallow(<Hero titleText='Lorem ipsum' />);
+    const component = shallow(<Hero titleText='Lorem ipsum' imageSrc='image.jpg'/>);
     expect(component).toBeTruthy();
     console.log(component.debug);
   });
@@ -14,11 +14,21 @@ describe('Component Hero', () => {
     expect(() => shallow(<Hero />)).toThrow();
   });
 
-  it('should render correct title', () => {
+  it('should render correct title and image', () => {
     const expectedTitle = 'Lorem ipsum';
-    const component = shallow(<Hero titleText={expectedTitle} />);
+    const expectedImage = 'image.ipg';
+    const component = shallow(<Hero titleText={expectedTitle} imageSrc={expectedImage}/>);
 
     const renderedTitle = component.find('.title').text();
     expect(renderedTitle).toEqual(expectedTitle);
+    expect(component.find('.image').prop('src')).toEqual(expectedImage);
+  });
+
+  it('renders correct classNames', () => {
+    const mockVariants = 'small dummy';
+    const component = shallow(<Hero titleText='Lorem ipsum' imageSrc='image.jpg' variant={mockVariants} />);
+    expect(component.hasClass('component')).toBe(true);
+    expect(component.hasClass('small')).toBe(true);
+    expect(component.hasClass('dummy')).toBe(true);
   });
 });
