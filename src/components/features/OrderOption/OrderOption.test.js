6a51983487dd2798ec9f3339edb8837ae5ce0a44
Magda Kwiatkowska-Witoń <magda-kwiatkowska@wp.pl>
2019-12-13 23:11:14 +0100

    Add tests for OrderOptionDropdown

--- src/components/features/OrderOption/OrderOption.test.js
+++ src/components/features/OrderOption/OrderOption.test.js
@@ -63,9 +63,10 @@ for (let type in optionTypes) {
     let component;
     let subcomponent;
     let renderedSubcomponent;
-    let mockSetOrderOption = jest.fn();
+    let mockSetOrderOption;
 
     beforeEach(() => {
+      mockSetOrderOption = jest.fn();
       component = shallow(<OrderOption type={type} setOrderOption={mockSetOrderOption} {...mockProps} {...mockPropsForType[type]} />);
       subcomponent = component.find(optionTypes[type]);
       renderedSubcomponent = subcomponent.dive();
@@ -81,6 +82,24 @@ for (let type in optionTypes) {
     switch (type) {
       case 'dropdown' : {
         /* tests for dropdown */
+        it('contains select and options', () => {
+          const select = renderedSubcomponent.find('select');
+          expect(select.length).toBe(1);
+
+          const emptyOption = renderedSubcomponent.find('option[value=""]').length;
+          expect(emptyOption).toBe(1);
+
+          const options = select.find('option').not('[value=""]');
+          expect(options.length).toBe(mockProps.values.length);
+          expect(options.at(0).prop('value')).toBe(mockProps.values[0].id);
+          expect(options.at(1).prop('value')).toBe(mockProps.values[1].id);
+        });
+
+        it('should run SetOrderOption function on change', () => {
+          renderedSubcomponent.find('select').simulate('change', {currentTarget: {value: testValue}});
+          expect(mockSetOrderOption).toBeCalledTimes(1);
+          expect(mockSetOrderOption).toBeCalledWith({ [mockProps.id]: testValue });
+        });
         break;
       }
     }
