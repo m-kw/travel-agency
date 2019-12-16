3e1e79bfd5c8536cf34e66cefa8cb9af7519081c
Magda Kwiatkowska-Witoń <magda-kwiatkowska@wp.pl>
2019-12-16 12:23:12 +0100

    Render OrderOptionDate to pass tests

--- src/components/features/OrderOption/OrderOptionDate.js
+++ src/components/features/OrderOption/OrderOptionDate.js
@@ -1,28 +1,25 @@
 import React from 'react';
 import DatePicker from 'react-datepicker';
+import PropTypes from 'prop-types';
 
 import 'react-datepicker/dist/react-datepicker-cssmodules.css';
 
 class OrderOptionDate extends React.Component {
-  state = {
-    startDate: new Date(),
-  }
-
-  handleChange = date => {
-    this.setState({
-      startDate: date,
-    });
-  }
-
   render() {
+    const { currentValue, setOptionValue } = this.props;
     return (
       <DatePicker
         dateFormat='dd MMMM yyyy'
-        selected={this.state.startDate}
-        onChange={this.handleChange}
+        selected={currentValue}
+        onChange={setOptionValue}
       />
     );
   }
 }
+
+OrderOptionDate.propTypes = {
+  currentValue: PropTypes.date,
+  setOptionValue: PropTypes.func,
+};
 
 export default OrderOptionDate;
