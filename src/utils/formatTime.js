771adcbc040b513abe99f0183d824383224703ff
Magda Kwiatkowska-Witoń <magda-kwiatkowska@wp.pl>
2019-12-21 21:59:09 +0100

    Check if formatTime returns time in hh:mm:ss if arg is proper

--- src/utils/formatTime.js
+++ src/utils/formatTime.js
@@ -1,10 +1,11 @@
-export const formatTime = (seconds) => {
-  if (seconds === undefined || typeof seconds === 'string' || typeof seconds === 'function') {
+export const formatTime = (arg) => {
+  if (arg === undefined || typeof arg === 'string' || typeof arg === 'function' || arg < 0) {
     return null;
-  }
-
-  if (seconds < 0) {
-    return null;
+  } else {
+    const seconds = Math.floor(arg % 60).toString();
+    const minutes = Math.floor((arg / 60) % 60).toString();
+    const hours = Math.floor(arg / 3600).toString();
+    return hours.padStart(2, '0') + ':' + minutes.padStart(2,'0') + ':' + seconds.padStart(2, '0');
   }
 
 };
--- src/utils/utils.test.js
+++ src/utils/utils.test.js
@@ -15,5 +15,12 @@ describe('utils', () => {
       expect(formatTime(-1)).toBe(null);
       expect(formatTime(-2)).toBe(null);
     });
+
+    it('should return time in hh:mm:ss if arg is proper', () => {
+      expect(formatTime(122)).toBe('00:02:02');
+      expect(formatTime(3793)).toBe('01:03:13');
+      expect(formatTime(120)).toBe('00:02:00');
+      expect(formatTime(3604)).toBe('01:00:04');
+    });
   });
 });
