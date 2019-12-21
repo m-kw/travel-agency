69758a44d8e131e361e2809a9277fd8343845ef2
Magda Kwiatkowska-Witoń <magda-kwiatkowska@wp.pl>
2019-12-21 23:34:21 +0100

    Check if there is no description if it is summer

--- src/components/features/DaysToSummer/DaysToSummer.js
+++ src/components/features/DaysToSummer/DaysToSummer.js
@@ -1,13 +1,34 @@
 import React from 'react';
-//import styles from './DaysToSummer';
+import PropTypes from 'prop-types';
+import styles from './DaysToSummer.scss';
 
 class DaysToSummer extends React.Component {
+  getDaysToSummer() {
+    const summerStart = new Date();
+    summerStart.setUTCMonth(5, 20);
+
+    const summerEnd = new Date();
+    summerEnd.setUTCMonth(8, 22);
+
+    const currentTime = new Date();
+
+    if (currentTime > summerStart || currentTime < summerEnd) {
+      return '';
+    }
+  }
+
   render() {
+    //const { title } = this.props;
     return(
-      <div>
+      <div className={styles.component}>
+        <h3 className={styles.title}>{this.getDaysToSummer()}</h3>
       </div>
     );
   }
 }
+
+DaysToSummer.propTypes = {
+  title: PropTypes.string,
+};
 
 export default DaysToSummer;
--- src/components/features/DaysToSummer/DaysToSummer.test.js
+++ src/components/features/DaysToSummer/DaysToSummer.test.js
@@ -2,9 +2,46 @@ import React from 'react';
 import DaysToSummer from './DaysToSummer';
 import { shallow } from 'enzyme';
 
+const mockProps = {
+  title: 'days to summer',
+};
+
+const trueDate = Date;
+const mockDate = customDate => class extends Date {
+  constructor(...args) {
+    if (args.length) {
+      super(...args);
+    } else {
+      super(customDate);
+    }
+    return this;
+  }
+  static now() {
+    return (new Date(customDate)).getTime();
+  }
+};
+
+const checkDescriptionAtDate = (date, expectedDescription) => {
+  it(`should show correct at ${date}`, () => {
+    global.Date = mockDate(`${date}T11:11:11.135Z`);
+
+    const component = shallow(<DaysToSummer {...mockProps}/>);
+    const renderedDays = component.find('.title').text();
+    expect(renderedDays).toEqual(expectedDescription);
+
+    global.Date = trueDate;
+  });
+};
+
 describe('Component DaysToSummer', () => {
   it('should render without crashing', () => {
     const component = shallow(<DaysToSummer />);
     expect(component).toBeTruthy();
   });
+});
+
+describe('Component DaysToSummer with mocked Date', () => {
+  checkDescriptionAtDate('2019-07-26', '');
+  checkDescriptionAtDate('2019-06-21', '');
+  checkDescriptionAtDate('2019-09-23', '');
 });
